809fa49f0530ff77bed7f7088a7d83a21a9204b6
lordtargarien <2021godblessme@gmail.com>
2025-04-11 03:14:30 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 import { Sparkles } from "lucide-react";
 import { motion } from "framer-motion";
 import Image from "next/image";
